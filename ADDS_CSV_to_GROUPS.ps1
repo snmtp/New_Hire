e2bc8f3fdbd7f1f88a939a4a89be16a164bbcf63
snmtp <93884000+snmtp@users.noreply.github.com>
2023-05-05 14:02:55 -0400

    Add files via upload

---
+++ ADDS_CSV_to_GROUPS.ps1
@@ -0,0 +1,26 @@
+﻿Connect-MgGraph
+#Connect-ExchangeOnline
+
+#Imports the csv file 
+$members = Import-Csv -Path 'C:\Users\SonNguyen\Desktop\2022AEAE.csv.csv' 
+$empty_array = @()
+foreach ($val in $members)
+{
+#write-output $val.name.toString();
+
+#--------- Appends member names to the array ----------
+$names = $val.Name.ToString()
+$empty_array += $names 
+}
+for($i = 0; $i -lt $empty_array.Length; $i++){
+   #splits first name and last name using the comma as the delimiter
+$split_array_name = $empty_array[$i].split(',')
+    #gets the name from the array's index 0 and 1 in the appropriate format: firstname.lastname@domain.com
+$full_mailname = $split_array_name[1] + '.' + $split_array_name[0] + '@domain.com'
+#write-output $full_mailname
+    #------- Adds every name to the group 2022aeae -------
+    #bypass switch is used to bypass owner check
+Add-DistributionGroupMember -Identity "2022AEAE" -Member $full_mailname.ToLower().trim() -BypassSecurityGroupManagerCheck  
+}
+
+
---
+++ ADD_TO_GROUPS.ps1
@@ -0,0 +1,43 @@
+﻿# Connect to Microsoft Graph and Exchange 
+Connect-MgGraph 
+Connect-ExchangeOnline 
+#Connect-MicrosoftTeams
+
+
+######## NOTES ####################################################
+######## ERROR: Current operation is not supported on GroupmailBox
+######## SOLUTION: use New-MgGroupMember instead of Add
+###################################################################
+
+[string]$user_first = Read-Host "First Name"
+[string]$user_last = Read-Host "Last name"
+
+$i = 0
+########## 20 is an arbitrary number ###################
+########################################################
+while($i -lt 20){
+    try { 
+    [string]$group_add = Read-Host "Which group(s) to add"
+    [string]$group_option = Read-Host "exchange-based or security (E or S)?"
+    $GroupId = (Get-MgGroup -Filter "DisplayName eq '$group_add'").Id
+    #do a if statement to check if group is (distro, mail-sec) or (security and 365) 
+        if ($group_option -eq "E"){
+        #The parameter member needs to be an EMAIL
+            Add-DistributionGroupMember -Identity "$group_add" -Member "$user_first.$user_last@domain.com" -BypassSecurityGroupManagerCheck 
+            Write-Host "$user_first $user_last successfully added! "
+                }
+        else{
+            New-MgGroupMember -GroupId $GroupId -DirectoryObjectId (Get-MgUser -UserId "$user_first.$user_last@domain.com").Id
+            Write-Host "Successfully added!"
+                }
+        $i++
+        }
+
+    catch {
+     Write-Host "Error Occured"
+     Write-Host $_
+    }
+}
+
+
+
